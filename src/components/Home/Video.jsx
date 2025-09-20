52389d45424cb0f40ae4e11b368e6dcff9b34c03
shr3y88 <shr3yanshtiwari@gmail.com>
2025-09-20 12:15:54 +0530

    Update Video.jsx

--- src/components/Home/Video.jsx
+++ src/components/Home/Video.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src='public/83c745cf.mp4'></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src='/83c745cf.mp4'></video>
     </div>
   )
 }
 
 
 export default Video
+
